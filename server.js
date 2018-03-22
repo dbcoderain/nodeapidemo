6991ce8bdd4ecf98cc417c1074f6f57cb332884c
David Boyd <david@coderain.co.uk>
2018-03-22 10:28:33 +0000

    update port to use env

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 
-const port = 3002;
+const port = process.env.PORT || 3002;
 
-// use it before all route definitions
-// app.use(cors({origin: 'http://localhost:5001'}));
 app.use(cors());
-
-//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 require('./app/routes')(app, {});
